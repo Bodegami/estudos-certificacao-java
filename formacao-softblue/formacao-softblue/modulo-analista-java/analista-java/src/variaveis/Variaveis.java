fefbb304036418bd15e3d3cb23709cc0e02c855a
Renato <rena.f.oliveira@gmail.com>
2023-01-02 22:58:13 -0300

    sobre variaveis

---
+++ formacao-softblue/formacao-softblue/modulo-analista-java/analista-java/src/variaveis/Variaveis.java
@@ -0,0 +1,34 @@
+package variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int numero = 50;
+		int numero2 = numero + 5;
+		
+		numero = numero - 1;
+		numero -= 1;
+		numero --;
+		
+		System.out.println(numero);
+		System.out.println(numero2);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		long l = 10L + 5;
+		double d = 5 * 2.0;
+		System.out.println(l);
+		System.out.println(d);
+		
+		double x = 0.0 / 0;
+		System.out.println(x);
+		
+		x = 10.0 / 0;
+		System.out.println(x);
+		
+		
+	}
+	
+}
