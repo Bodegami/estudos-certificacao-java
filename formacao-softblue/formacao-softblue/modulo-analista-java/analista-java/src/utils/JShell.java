9b3acf82771e80e778628931d5f596d1f5e61790
Renato <rena.f.oliveira@gmail.com>
2023-01-03 23:06:13 -0300

    sobre o jShell

---
+++ formacao-softblue/formacao-softblue/modulo-analista-java/analista-java/src/utils/JShell.java
@@ -0,0 +1,44 @@
+package utils;
+
+public class JShell {
+
+	public static void main(String[] args) {
+		
+		// É acesso atraves do terminal do computador pelo comando "jshell", desde que a variavel "JAVA_HOME" esteja 
+		//configurada na máquina.
+		
+		// o comando /exit sai do jshell.
+		
+		// jshell -v é o modo verboso
+		
+		// ao executar 1 + 1, o jshell cria uma variavel temporaria e armazena o resultado da expressão
+		
+		// o comando /vars exibe todas as variaveis criadas
+		
+		// é possivel criar metodos e classes
+		
+		// é possivel sobrescrever um metodo ou classe apenas redefinindo o mesmo
+		
+		// o comando /imports imprime os imports feitos ate o momento
+		
+		// o comando /open DEFAULT importa os pacotes basicos do Java como .utils
+		
+		// o comando /set start DEFAULT -retain salva definitivamente os imports no jShell
+		
+		// o comando /methods imprime os metodos criados ate o momento
+		
+		// o comando /open PRINTING importa alguns metodos de print na janela do jshell
+		
+		// o comando /set start PRINTING -retain importa de forma definitiva os metodos de print no jshell
+		
+		// o comando /set feedback imprime os tipos de feedback disponiveis
+		
+		// o comando /set feedback verbose - retain define o feedback verbose de forma definitiva no jshell
+		
+		// o comando /help imprime todos comando disponiveis
+		
+		// o comando /help "comando" imprime todas opcoes disponiveis do comando informado
+		
+	}
+	
+}
